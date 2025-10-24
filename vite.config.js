4bc963cce044d0bf2dafe6988b36c0a127094c64
bbacelar <brunobacelar88@gmail.com>
2025-10-23 22:40:04 -0300

    refactor: use gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Altere o valor de base para corresponder ao nome do repositório GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/3d-print-cost-app/'
+  base: '/3dprint-cost-calculator/'
 })
